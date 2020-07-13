b2848ddb92e704c315026516f6d77f63281cec48
Henrik Fricke <henrikfricke@web.de>
2020-07-13 05:42:03 +0200

    refactor(frame): unify padding handling

--- src/shared/ui/Frame/Frame.tsx
+++ src/shared/ui/Frame/Frame.tsx
@@ -3,12 +3,7 @@ import { Box, Logo } from '@srvycool/ds';
 
 export const Frame: React.FC = ({ children }) => {
   return (
-    <Box
-      paddingTop={['m', 'm', 'xl']}
-      paddingX={['m', 'm', 'xxl']}
-      marginBottom="xl"
-      maxWidth="720px"
-    >
+    <Box padding={['m', 'm', 'xl']} maxWidth="680px">
       <Box marginBottom="xl">
         <Logo />
       </Box>
